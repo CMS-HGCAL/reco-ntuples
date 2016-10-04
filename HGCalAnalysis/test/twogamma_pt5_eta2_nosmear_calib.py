97d6abcf04554dcd37504576950c3433283b17dc
Clemens Lange <clemens.lange@cern.ch>
2016-10-04 17:13:15 +0200

    update geometry for pre12

--- HGCalAnalysis/test/twogamma_pt5_eta2_nosmear_calib.py
+++ HGCalAnalysis/test/twogamma_pt5_eta2_nosmear_calib.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from Configuration.StandardSequences.Eras import eras
 
 process = cms.Process("Demo")
-process.load('Configuration.Geometry.GeometryExtended2023simReco_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D3Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
