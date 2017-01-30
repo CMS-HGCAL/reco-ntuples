21655e250b3b1a0006afc15f3797c5dc1d27cc0d
Florian Beaudette <Florian.Beaudette@cern.ch>
2017-01-30 23:58:45 +0100

    added file from web interface as this !@$~@$~@% git refuses to push my change

---
+++ HGCalAnalysis/test/singleEle15.py
@@ -0,0 +1,55 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process("Demo")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+from FastSimulation.Event.ParticleFilter_cfi import *
+#process.load("RecoLocalCalo.HGCalRecProducers.hgcalLayerClusters_cfi")
+
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        '/store/relval/CMSSW_8_2_0_patch1/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/90X_upgrade2023_realistic_v1_2023D4-v1/10000/0E600BFE-A3E2-E611-8D74-0\
+CC47A4C8E14.root',
+        '/store/relval/CMSSW_8_2_0_patch1/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/90X_upgrade2023_realistic_v1_2023D4-v1/10000/20D4831D-A4E2-E611-8E46-0\
+CC47A4C8F30.root',
+        '/store/relval/CMSSW_8_2_0_patch1/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/90X_upgrade2023_realistic_v1_2023D4-v1/10000/5E8E5EFB-A2E2-E611-9508-0\
+CC47A4D76AC.root',
+        '/store/relval/CMSSW_8_2_0_patch1/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/90X_upgrade2023_realistic_v1_2023D4-v1/10000/68478B06-A3E2-E611-8626-0\
+025905A48EC.root'
+    ),
+)
+
+
+
+process.ana = cms.EDAnalyzer('HGCalAnalysis',
+                             detector = cms.string("all"),
+                             rawRecHits = cms.bool(True),
+                             readOfficialReco = cms.bool(True),
+                             readCaloParticles = cms.bool(False),
+                             TestParticleFilter = ParticleFilterBlock.ParticleFilter
+                             )
+
+#Quite important
+process.ana.TestParticleFilter.protonEMin = cms.double(100000)
+process.ana.TestParticleFilter.etaMax = cms.double(3.1)
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string("singleElePt15.root")
+
+                                   )
+#process.imagingClusterHGCal.ecut = cms.double(0.01)
+#process.imagingClusterHGCal.eventsToDisplay = cms.untracked.uint32(2)
+
+#process.p = cms.Path(process.hgcalLayerClusters+process.ana)
+process.p = cms.Path( process.ana)
