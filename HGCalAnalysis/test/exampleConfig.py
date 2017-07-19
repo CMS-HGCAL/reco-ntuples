b8cd5b95e49f36fce87237724bd0c7ee683dee29
Florian Beaudette <Florian.Beaudette@cern.ch>
2017-07-19 11:45:57 +0200

    restore master example settings

--- HGCalAnalysis/test/exampleConfig.py
+++ HGCalAnalysis/test/exampleConfig.py
@@ -4,14 +4,14 @@ from Configuration.StandardSequences.Eras import eras
 process = cms.Process("Demo")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
-#process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
-process.load('Configuration.Geometry.GeometryExtended2023D17Reco_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.Geometry.GeometryExtended2023D13Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('RecoLocalCalo.HGCalRecProducers.HGCalLocalRecoSequence_cff')
-
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 from FastSimulation.Event.ParticleFilter_cfi import *
 from RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi import dEdX_weights as dEdX
 
@@ -20,15 +20,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        #        '/store/relval/CMSSW_9_1_0_pre1/RelValTTbar_14TeV/GEN-SIM-RECO/PU25ns_90X_upgrade2023_realistic_v9_D4TPU200r3-v1/00000/02E3478F-3D22-E711-80C4-0CC47A4D7654.root'
-#        'file:0EDF6499-AF35-E711-8039-0CC47A7C3428.root',
-        '/store/relval/CMSSW_9_1_0_pre3/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmear-v1/10000/0EDF6499-AF35-E711-8039-0CC47A7C3428.root',
-        '/store/relval/CMSSW_9_1_0_pre3/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmear-v1/10000/36ED744B-B235-E711-ADE1-0025905A6084.root',
-        '/store/relval/CMSSW_9_1_0_pre3/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmear-v1/10000/4436C35F-B235-E711-8F37-0025905A6090.root',
-        '/store/relval/CMSSW_9_1_0_pre3/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmear-v1/10000/5A6B4153-AF35-E711-A958-0CC47A745298.root',
-        '/store/relval/CMSSW_9_1_0_pre3/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmear-v1/10000/5CF20899-AF35-E711-A952-0025905A60BE.root',
-        '/store/relval/CMSSW_9_1_0_pre3/RelValDoubleElectronPt15Eta17_27/GEN-SIM-RECO/91X_upgrade2023_realistic_v1_D13noSmear-v1/10000/88C22846-AF35-E711-BC3D-0CC47A7C346E.root'
-
+        '/store/relval/CMSSW_9_1_0_pre1/RelValTTbar_14TeV/GEN-SIM-RECO/PU25ns_90X_upgrade2023_realistic_v9_D4TPU200r3-v1/00000/02E3478F-3D22-E711-80C4-0CC47A4D7654.root'
     ),
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck")
 )
@@ -47,7 +39,7 @@ process.ana.TestParticleFilter.protonEMin = cms.double(100000)
 process.ana.TestParticleFilter.etaMax = cms.double(3.1)
 
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string("hgcalNtuple-test.root")
+                                   fileName = cms.string("hgcalNtuple.root")
 
                                    )
 
