86eef75a3eb043d7d1b439f253038ff43ecbd4de
Clemens Lange <clemens.lange@cern.ch>
2017-08-28 11:58:35 +0200

    update to pre4 file and limit to 10 events

--- HGCalAnalysis/test/exampleConfig.py
+++ HGCalAnalysis/test/exampleConfig.py
@@ -15,12 +15,12 @@ process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
 from FastSimulation.Event.ParticleFilter_cfi import *
 from RecoLocalCalo.HGCalRecProducers.HGCalRecHit_cfi import dEdX_weights as dEdX
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        '/store/relval/CMSSW_9_3_0_pre3/RelValTTbar_14TeV/GEN-SIM-RECO/PU25ns_92X_upgrade2023_realistic_v2_D17PU200-v2/00000/005D7BA7-A97E-E711-A489-E0071B74AD00.root'
+        '/store/relval/CMSSW_9_3_0_pre4/RelValTTbar_14TeV/GEN-SIM-RECO/PU25ns_93X_upgrade2023_realistic_v0_D17PU200-v1/00000/082041D3-1E89-E711-80AA-0242AC130002.root'
     ),
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck")
 )
